b0519fdae58146786fb68ecc09e7e7b54fa3e880
Yilian90 <chenyilian90@gmail.com>
2023-11-08 14:39:58 +0800

    insert background wallpaper, create layout for login page with login button as anonymous user

--- app/src/main/java/com/example/chatapp/views/loginActivity.java
+++ app/src/main/java/com/example/chatapp/views/loginActivity.java
@@ -1,14 +1,14 @@
-package com.example.chatapp;
+package com.example.chatapp.views;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import com.example.chatapp.R;
 
-public class MainActivity extends AppCompatActivity {
+public class loginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
