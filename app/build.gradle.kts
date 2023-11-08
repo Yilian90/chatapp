142ab5d923306ef524a0f0d200b3eb1de2597309
Yilian90 <chenyilian90@gmail.com>
2023-11-08 15:51:06 +0800

    create chatGroup with constructor, getter and setter function

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.chatapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.chatapp"
--- app/src/main/java/com/example/chatapp/views/loginActivity.java
+++ app/src/main/java/com/example/chatapp/views/loginActivity.java
@@ -1,14 +1,31 @@
 package com.example.chatapp.views;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.ViewModelProvider;
+
 import android.os.Bundle;
 import com.example.chatapp.R;
+import com.example.chatapp.databinding.ActivityLoginBinding;
+import com.example.chatapp.viewmodel.MyViewModel;
 
 public class loginActivity extends AppCompatActivity {
 
+    MyViewModel viewModel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        viewModel = new ViewModelProvider(this).get(MyViewModel.class);
+        //ViewModelProvider is responsible for creating and managing view model instances
+        //get method to retrieves the an instance of the view model class (MyViewModel)
+        //The ViewModelProvide will create a new instance of MyViewModel if one it doesn't exist for the
+        //associated component, or it will return the existing instance if it already exists.
+
+        ActivityLoginBinding activityLoginBinding =
+                DataBindingUtil.setContentView(this, R.layout.activity_login);
+
+        activityLoginBinding.setVModel(viewModel);
     }
 }
