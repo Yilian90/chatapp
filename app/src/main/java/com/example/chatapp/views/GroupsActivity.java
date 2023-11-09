a266b5b9a7a6e85d9625e5a42caaabcd45cdccde
Yilian90 <chenyilian90@gmail.com>
2023-11-09 15:22:18 +0800

    define viewmodel, create recyclerView with data binding, setup observer

--- app/src/main/java/com/example/chatapp/views/GroupsActivity.java
+++ app/src/main/java/com/example/chatapp/views/GroupsActivity.java
@@ -1,19 +1,67 @@
 package com.example.chatapp.views;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 import com.example.chatapp.R;
+import com.example.chatapp.databinding.ActivityGroupsBinding;
+import com.example.chatapp.model.ChatGroup;
+import com.example.chatapp.viewmodel.MyViewModel;
+import com.example.chatapp.views.adapters.GroupAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GroupsActivity extends AppCompatActivity {
 
-
     //get all the groups available in Firebase Realtime DB
+    private ArrayList<ChatGroup> chatGroupArrayList;
+
+    private RecyclerView recyclerView;
+    private GroupAdapter groupAdapter;
+    private ActivityGroupsBinding binding;
+    private MyViewModel myViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_groups);
+
+        binding = DataBindingUtil.setContentView(
+                this,
+                R.layout.activity_groups
+        );
+
+        //Define the ViewModel
+        myViewModel = new ViewModelProvider( this).get(MyViewModel.class);
+
+
+        //RecycleView With Data Binding
+        recyclerView = binding.recyclerView;
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        // Setup an observer to listen for changes in a "Live Data" object
+        myViewModel.getGroupList().observe(this, new Observer<List<ChatGroup>>() {
+            @Override
+            public void onChanged(List<ChatGroup> chatGroups) {
+                // the updated data is received as "chatGroups" parameter in onChanged()
+
+                //display the chat group
+                chatGroupArrayList = new ArrayList<>();
+                chatGroupArrayList.addAll(chatGroups);
+
+                groupAdapter = new GroupAdapter(chatGroupArrayList);
+
+                recyclerView.setAdapter(groupAdapter);
+                groupAdapter.notifyDataSetChanged();
+            }
+        });
+
     }
 }
