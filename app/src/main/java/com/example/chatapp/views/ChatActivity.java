7ca149bf4e4bf9ffe7b78696382ba5ab6cb1f84f
Yilian90 <chenyilian90@gmail.com>
2023-12-06 09:52:38 +0800

    done observe live data

--- app/src/main/java/com/example/chatapp/views/ChatActivity.java
+++ app/src/main/java/com/example/chatapp/views/ChatActivity.java
@@ -1,16 +1,69 @@
 package com.example.chatapp.views;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 import com.example.chatapp.R;
+import com.example.chatapp.databinding.ActivityChatBinding;
+import com.example.chatapp.model.ChatMessage;
+import com.example.chatapp.viewmodel.MyViewModel;
+import com.example.chatapp.views.adapters.ChatAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ChatActivity extends AppCompatActivity {
+    private ActivityChatBinding binding;
+    private MyViewModel myViewModel;
+    private RecyclerView recyclerView;
+    private ChatAdapter myAdapter;
+
+    private List<ChatMessage> messagesList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_chat);
+
+        myViewModel = new ViewModelProvider(this).get(MyViewModel.class);
+
+        //RecyclerView with DataBinding
+        recyclerView = binding.recyclerView;
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+
+        //Getting the Group Name from the Clicked Item in the GroupsActivity
+        String groupName = getIntent().getStringExtra("GROUP_NAME");
+
+        myViewModel.getMessagesLiveData(groupName).observe(this, new Observer<List<ChatMessage>>() {
+            @Override
+            public void onChanged(List<ChatMessage> chatMessages) {
+                //to listen to the updated chat messages.
+                messagesList = new ArrayList<>();
+                messagesList.addAll(chatMessages); //updated to the lastest updates in the chat messages.
+
+                myAdapter = new ChatAdapter(messagesList, getApplicationContext());
+
+                recyclerView.setAdapter(myAdapter);
+                myAdapter.notifyDataSetChanged();
+
+                //Scroll to the latest message added:
+                int latestPosition = myAdapter.getItemCount() -1;
+                recyclerView.smoothScrollToPosition(latestPosition);
+                //Forcing the recyclerView to scroll to the latest message added.
+
+            }
+        });
+        binding.setVModel(myViewModel);
+        //use data binding.set view model to this view model, passing this variable to MyViewModel inside activity.chat.xml
     }
 }
